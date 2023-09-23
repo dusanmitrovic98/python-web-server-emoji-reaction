f58bb203f23d2326c16cb9619f78d919fece6c2f
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-23 20:50:36 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,3 +2,4 @@ function showEmoji(emoji) {
   const emojiPopup = document.getElementById("emojiPopup");
   const emojiElement = document.createElement("div");
   emojiElement.className = "emoji";
+  emojiElement.innerText = emoji;
