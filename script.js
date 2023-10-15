de6202b1eaf795dea692c0927fe6e6668832447a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-10-15 15:36:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function showEmoji(emoji) {
-    const emojiPopup = document.getElementById('emojiPopup');
-    emojiPopup.textContent = emoji;
-    emojiPopup.style.display = 'block';
-
-    setTimeout(function () {
-        emojiPopup.style.display = 'none';
-    }, 3000); // Display emoji for 3 seconds
+  const emojiPopup = document.getElementById("emojiPopup");
+  emojiPopup.textContent = emoji;
+  emojiPopup.style.display = "block";
 }
